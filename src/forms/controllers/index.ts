898e65be3a5f2390588eab4559e9d9e2f274d8e1
Konrad Duleba <kon.duleba@gmail.com>
2025-04-04 23:06:23 +0200

    feat: Test new importts

--- src/forms/controllers/index.ts
+++ src/forms/controllers/index.ts
@@ -1,4 +1,1 @@
-export * from "./form-single-select/form-single-select";
-export * from "./form-input-number/form-input-number";
-export * from "./form-checkbox/form-checkbox";
 export * from "./form-text-input";
